d9224a64775d06b0cb9085b3457109afb574bbcc
MariaRez <maria.v.reznik@yandex.ru>
2023-03-01 13:59:08 +0300

    fix: change cinematic in Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,18 +7,18 @@ import Navigation from '../Navigation/Navigation';
 function Header() {
   return (
     <header className='header'>
-            <div className='social-media-links'>
-                <a className='social-media-link' href="https://wa.me/qr/MMWAREFAW4N4O1">
-                  <img className='social-media-link-image' src={whatsApp} alt='WhatsApp'/>
-                </a>
-                <a className='social-media-link' href="https://www.instagram.com/m.sha.aa">
-                  <img className='social-media-link-image' src={instagram} alt='Instagram'/>
-                </a>
-                <a className='social-media-link' href="https://www.facebook.com/mariiareznik">
-                  <img className='social-media-link-image' src={facebook} alt='Facebook'/>
-                </a>
-            </div>
-            <Navigation />
+      <address className='social-media-links'>
+        <a className='social-media-link' href="https://wa.me/qr/MMWAREFAW4N4O1">
+          <img className='social-media-link-image' src={whatsApp} alt='WhatsApp'/>
+        </a>
+        <a className='social-media-link' href="https://www.instagram.com/m.sha.aa">
+          <img className='social-media-link-image' src={instagram} alt='Instagram'/>
+        </a>
+        <a className='social-media-link' href="https://www.facebook.com/mariiareznik">
+          <img className='social-media-link-image' src={facebook} alt='Facebook'/>
+        </a>
+      </address>
+      <Navigation />
     </header>
   );
 }
