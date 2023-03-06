1d8cd53ee71128745090b5ddf568bf12dbd1882c
MariaRez <maria.v.reznik@yandex.ru>
2023-03-06 14:21:26 +0300

    fix: ul in header social-links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,16 +7,24 @@ import Navigation from '../Navigation/Navigation';
 function Header() {
   return (
     <header className='header'>
-      <address className='social-media-links'>
-        <a className='social-media-link' href="https://wa.me/qr/MMWAREFAW4N4O1" target='_blank' rel="noreferrer">
-          <img className='social-media-link-image' src={whatsApp} alt='WhatsApp'/>
-        </a>
-        <a className='social-media-link' href="https://www.instagram.com/m.sha.aa" target='_blank' rel="noreferrer">
-          <img className='social-media-link-image' src={instagram} alt='Instagram'/>
-        </a>
-        <a className='social-media-link' href="https://www.facebook.com/mariiareznik" target='_blank' rel="noreferrer">
-          <img className='social-media-link-image' src={facebook} alt='Facebook'/>
-        </a>
+      <address>
+        <ul className='social-media-links'>
+          <li>
+            <a className='social-media-link' href="https://wa.me/qr/MMWAREFAW4N4O1" target='_blank' rel="noreferrer">
+              <img className='social-media-link-image' src={whatsApp} alt='WhatsApp'/>
+            </a>
+          </li>
+          <li>
+            <a className='social-media-link' href="https://www.instagram.com/m.sha.aa" target='_blank' rel="noreferrer">
+              <img className='social-media-link-image' src={instagram} alt='Instagram'/>
+            </a>
+          </li>
+          <li>
+            <a className='social-media-link' href="https://www.facebook.com/mariiareznik" target='_blank' rel="noreferrer">
+              <img className='social-media-link-image' src={facebook} alt='Facebook'/>
+            </a>
+          </li>
+        </ul>
       </address>
       <Navigation />
     </header>
