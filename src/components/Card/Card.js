10032ec250eb7a85b1e665e2407f43745615329e
MariaRez <maria.v.reznik@yandex.ru>
2023-03-07 20:31:44 +0300

    feat: cardlist - grid, modification in card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,23 +1,10 @@
 import './Card.css';
 
-function Card({cat1, cat2, title, description, time, tag1, tag2, image}) {
+function Card({ title, image }) {
     return (
        <div className='card'>
-        <div className='card__right-column'>
-            <ul className='card-categories'>
-              <li className='card-categories__item'>{cat1}</li>
-              <li className='card-categories__item'>{cat2}</li>
-          </ul>
-          <h4 className='card-title'>{title}</h4>
-          <p className='card-description'>{description}</p>
-          <span className='card-time-text'>Дата публикации: <time className='card-time' dateTime={time}>{time}</time></span>
-          <ul className='card-tags'>
-             <span className='card-tags__title'>теги:</span>
-              <li className='card-tag'>{tag1}</li>
-              <li className='card-tag'>{tag2}</li>
-          </ul>
-        </div>
         <img className='card-image'src={image} alt={title}/>
+        <h4 className='card-title'>{title}</h4>
        </div>
     );
   }
--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -8,37 +8,49 @@ function CardList({title}) {
             <ul className='card-list__items'>
                 <li>
                     <Card 
-                      cat1={'Завтраки'}
-                      cat2={'Cытное'}
                       title={'Завтрак Словенский'}
-                      description={'Простое блюдо, которое насытит вас и ваших близких. Готовиться довольно просто, но вкус просто потрясающий'}
-                      time={'2021-10-12'}
-                      tag1={'яйца'}
-                      tag2={'сытное'}
                       image={'https://eda.ru/img/eda/c1240x830/s1.eda.ru/StaticContent/Photos/161125164152/161209123709/p_O.jpg.webp'}
                     />
                 </li>
                 <li>
                     <Card 
-                      cat1={'Завтраки'}
-                      cat2={'Легкое'}
                       title={'Оладьи на йогурте к завтраку'}
-                      description={'Быстрые и вкусные оладьи из пары ингредиентов'}
-                      time={'2022-11-01'}
-                      tag1={'яйца'}
-                      tag2={'легкое'}
                       image={'https://eda.ru/img/eda/c1240x830/s1.eda.ru/StaticContent/Photos/120214130615/161112150502/p_O.jpg.webp'}
                     />
                 </li>
                 <li>
                     <Card 
-                      cat1={'Завтраки'}
-                      cat2={'Сытное'}
-                      title={'Завтрак из McDonald’s. Макмаффин с яйцом и свиной котлетой'}
-                      description={'Давно не были в любимом фаст-фуд ресторане? Ах, да, он же закрыт! Тогда самое время сделать самому любимый бургер на завтрак'}
-                      time={'2023-03-02'}
-                      tag1={'яйца'}
-                      tag2={'сыр'}
+                      title={'Завтрак из McDonald’s'}
+                      image={'https://eda.ru/img/eda/c1240x830/s1.eda.ru/StaticContent/Photos/121220162751/150118205557/p_O.jpg.webp'}
+                    />
+                </li>
+                <li>
+                    <Card 
+                      title={'Завтрак Словенский'}
+                      image={'https://eda.ru/img/eda/c1240x830/s1.eda.ru/StaticContent/Photos/161125164152/161209123709/p_O.jpg.webp'}
+                    />
+                </li>
+                <li>
+                    <Card 
+                      title={'Оладьи на йогурте к завтраку'}
+                      image={'https://eda.ru/img/eda/c1240x830/s1.eda.ru/StaticContent/Photos/120214130615/161112150502/p_O.jpg.webp'}
+                    />
+                </li>
+                <li>
+                    <Card 
+                      title={'Завтрак из McDonald’s'}
+                      image={'https://eda.ru/img/eda/c1240x830/s1.eda.ru/StaticContent/Photos/121220162751/150118205557/p_O.jpg.webp'}
+                    />
+                </li>
+                <li>
+                    <Card 
+                      title={'Оладьи на йогурте к завтраку'}
+                      image={'https://eda.ru/img/eda/c1240x830/s1.eda.ru/StaticContent/Photos/120214130615/161112150502/p_O.jpg.webp'}
+                    />
+                </li>
+                <li>
+                    <Card 
+                      title={'Завтрак из McDonald’s'}
                       image={'https://eda.ru/img/eda/c1240x830/s1.eda.ru/StaticContent/Photos/121220162751/150118205557/p_O.jpg.webp'}
                     />
                 </li>
