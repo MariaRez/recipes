85ec3e106420eb084b064d98c82774147852ec2b
MariaRez <maria.v.reznik@yandex.ru>
2023-03-21 22:44:45 +0300

    feat: start work with main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,17 @@
 import './Main.css';
+import glass from '../../images/main/glass.jpg';
+import bread from '../../images/main/bread-and-eggs.jpg';
+import sky from '../../images/main/sky.jpg';
 
 function Main() {
   return (
     <main className='main'>
-      Главная страница
+      <div className='main__image-container'>
+        <img className='main__image' alt='' src={glass}></img>
+        <img className='main__image' alt='' src={bread}></img>
+        <img className='main__image' alt='' src={sky}></img>
+      </div>
+      <span className="main-text">Давайте познакомимся</span>
     </main>
   );
 }
