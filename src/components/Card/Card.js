955344d703c0336eb6eed32fbd7529d69c4fe33f
MariaRez <maria.v.reznik@yandex.ru>
2023-03-27 19:07:05 +0300

    feat: recipes page

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,20 @@
-import { Link } from 'react-router-dom';
-import './Card.css';
+import { Link } from "react-router-dom";
+import "./Card.css";
 
-function Card() {
-    return (
-      <Link className='card' to='/recipe'>
-         <div className="card__overlay"></div>
-         <h3 className='card__title'>Турецкий завтрак</h3>
-         <p className='card__description'>Kahvaltı – именно так звучит «завтрак» по-турецки. Название происходит от слов «kahve» (кофе) и «altı» (перед), что можно трактовать примерно как «еда перед кофе». </p>
-      </Link>
-    );
-  }
-  
-  export default Card;
+function Card({ cardTitle, cardDescription, to, image }) {
+  return (
+    <Link
+      className="card"
+      to={to}
+      style={{
+        backgroundImage: `url(${image})`,
+      }}
+    >
+      <div className="card__overlay"></div>
+      <h3 className="card__title">{cardTitle}</h3>
+      <p className="card__description">{cardDescription} </p>
+    </Link>
+  );
+}
+
+export default Card;
--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,23 +1,25 @@
-import Card from '../Card/Card';
-import './CardList.css';
+import Card from "../Card/Card";
+import "./CardList.css";
+import taco from "../../images/recipes/taco.jpeg";
 
-function CardList({title}) {
-    return (
-        <section className='card-list section'>
-            <h3 className='card-list__title'>Категория: {title}</h3>
-            <ul className='card-list__items'>
-                <li><Card/></li>
-                <li><Card/></li>
-                <li><Card/></li>
-                <li><Card/></li>
-                <li><Card/></li>
-                <li><Card/></li>
-                <li><Card/></li>
-                <li><Card/></li>
-                <li><Card/></li>
-            </ul>
-        </section>
-    );
-  }
-  
-  export default CardList;
+function CardList({ title }) {
+  return (
+    <section className="card-list section">
+      <h3 className="card-list__title">Категория: {title}</h3>
+      <ul className="card-list__items">
+        <li>
+          <Card
+            cardTitle={"Тако с растительным фаршем"}
+            cardDescription={
+              "Быстрое и простое блюдо, из минимума ингредиентов, которое подойдет как для плотного обеда так и для сытного ужина"
+            }
+            to={"/recipe"}
+            image={taco}
+          />
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default CardList;
