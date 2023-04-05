057d40196dc6d518951afad6dd2a9dd6bec2e6e9
MariaRez <maria.v.reznik@yandex.ru>
2023-04-05 12:00:01 +0300

    fix errors

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 import './index.css';
-// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
 import reportWebVitals from './reportWebVitals';
 
 // eslint-disable-next-line no-undef
